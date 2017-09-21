474d7aa2c846181d76812125e19f91d2bc3b6e7e
Olga Starkova <olstar.ru@gmail.com>
2017-09-21 12:48:35 +0200

    devide settings changed

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,5 +1,6 @@
 class PoemsController < ApplicationController
 	before_action :set_poem, only: [:show, :edit, :update, :destroy]
+	skip_before_action :authenticate_user!, only: [ :index ]
 
 	def index
 		@poem = Poem.new
