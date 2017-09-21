c04a80b3162e2ce79e66f30e4dd3c75ca9c3eb7e
Olga Starkova <olstar.ru@gmail.com>
2017-09-21 17:10:06 +0200

    test debug

--- app/admin/poems.rb
+++ app/admin/poems.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register Poem do
 	permit_params :title, :user_id, :content, :year, :dedicated_to, :cover
+
+	index do
+    selectable_column
+    column :id
+    column :title
+    column :user
+    column :year
+    column :cover
+    column :dedicated_to
+    actions
+	end
 end
 
